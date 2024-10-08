cee97967759f08a31122347d8ba32336383bdc67
HyeongJinK <wkzkfmxk23@gmail.com>
2024-10-08 18:57:17 +0900

    기본 테스트 코드

---
+++ README.md
@@ -0,0 +1,6 @@
+```bash
+cargo new [project_name] --bin
+cargo build
+cargo run
+cargo check      # 컴파일만 확인
+```
--- hello_cargo/target/debug/hello_cargo.d
+++ hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/Users/hyeongjinkim/Desktop/project/study/rust/hello_cargo/target/debug/hello_cargo: /Users/hyeongjinkim/Desktop/project/study/rust/hello_cargo/src/main.rs
+/Users/oysterable/Desktop/project/khj_rust/hello_cargo/target/debug/hello_cargo: /Users/oysterable/Desktop/project/khj_rust/hello_cargo/src/main.rs
